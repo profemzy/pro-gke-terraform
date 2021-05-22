6e65ace214d43c0bf2838886987b433c326b5e53
Femi Oladele <profemzy@gmail.com>
2021-05-22 22:37:15 +0100

    GKE Private Cluster with Auth Network

---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+project_id = "invl-cowries"
+region     = "europe-west3"
+
+cluster_secondary_range_name = "pods"
+cluster_secondary_range_cidr = "10.16.0.0/12"
+services_secondary_range_name = "services"
+services_secondary_range_cidr = "10.1.0.0/20"
---
+++ vpc.tf
@@ -0,0 +1,43 @@
+variable "project_id" {
+  description = "project id"
+}
+
+variable "region" {
+  description = "region"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+
+# VPC
+resource "google_compute_network" "vpc" {
+  name                    = "${var.project_id}-vpc"
+  auto_create_subnetworks = "false"
+}
+
+# Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.project_id}-subnet"
+  region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.0.16.0/20"
+
+  secondary_ip_range {
+    range_name    = var.cluster_secondary_range_name
+    ip_cidr_range = var.cluster_secondary_range_cidr
+  }
+
+  secondary_ip_range {
+    range_name    = var.services_secondary_range_name
+    ip_cidr_range = var.services_secondary_range_cidr
+  }
+
+  private_ip_google_access = true
+
+  depends_on = [
+    google_compute_network.vpc,
+  ]
+}
