58244fa779679daedf8896eb351ea5594dfd3086
Femi Oladele <profemzy@gmail.com>
2021-06-13 01:36:43 +0100

    Allow p81 IP access to cluster

--- gke.tf
+++ gke.tf
@@ -23,8 +23,8 @@ resource "google_container_cluster" "primary" {
 
   master_authorized_networks_config {
     cidr_blocks {
-      cidr_block   = "154.118.16.149/32"
-      display_name = "InfoTitans"
+      cidr_block   = "212.59.65.187/32"
+      display_name = "Perimeter81"
     }
   }
 
